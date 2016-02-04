4f4fc18a0f1ee8e38e0864b8b5a45660f0ffb9fb
marianelagaite <marian.gaite92@gmail.com>
2016-02-04 07:16:15 -0300

    Agregadas las rutas de people y clients

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :clients
+  resources :people
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
