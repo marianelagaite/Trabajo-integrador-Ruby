a3e4474324227ecd5ae15c5fe56e7c95ab0d3a2f
marianelagaite <marian.gaite92@gmail.com>
2016-02-04 12:43:35 -0300

    Se agregaron los métodos toTrash, restore, self.deleted, self.notDeleted al modelo de persona

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,16 @@
 class Person < ActiveRecord::Base
-	has_many :clients 
+	def toTrash
+		self.update(trash: 1)
+	end
+	def restore
+		self.update(trash: 0)
+	end
+
+	def self.deleted
+		Person.where(trash: 1)
+	end
+
+	def self.notDeleted
+		Person.where(trash: 0)
+	end
 end
