2081bbbc9c9554ada3181e8230388efcd03b49c1
marianelagaite <marian.gaite92@gmail.com>
2016-02-11 12:30:59 -0300

    Se modificó para que elimine en cacada sus bills

--- app/models/purchaser.rb
+++ app/models/purchaser.rb
@@ -1,14 +1,14 @@
 class Purchaser < ActiveRecord::Base
 	belongs_to :person
-	has_many :bills, inverse_of: :purchaser
+	has_many :bills, inverse_of: :purchaser, dependent: :delete_all
 
 	accepts_nested_attributes_for :person
 
 	validates :person, presence: true
 	validates :cuil_cuit, {presence: true,
-								uniqueness: true,
+							uniqueness: true,
 								length: { is: 11}}
-
+								
 	def nameSurname
 		self.person.nameSurname
 	end
