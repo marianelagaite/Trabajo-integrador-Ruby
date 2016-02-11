021bf8bddfab2de2c3deb22f7078a29247d55e31
marianelagaite <marian.gaite92@gmail.com>
2016-02-11 02:25:50 -0300

    agregado helper link_to_add_fields

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	#f = form builder
+	def link_to_add_fields(name, f, association)
+		new_obj = f.object.send(association).klass.new
+		id = new_obj.object_id
+		fields = f.fields_for(association, new_obj, child_index: id) do |b|
+			render(association.to_s.singularize + "_fields", f: b)
+		end
+		link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
+	end
 end
