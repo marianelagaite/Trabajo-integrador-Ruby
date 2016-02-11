a6b7d603164f2e2ebe6f7b88b253af7d61b3e46b
marianelagaite <marian.gaite92@gmail.com>
2016-02-11 12:17:22 -0300

    Segregaron las relaciones y las validaciones del modelo de persona

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,22 @@
 class Person < ActiveRecord::Base
+	has_one :client, inverse_of: :person 
+	has_one :purchaser, inverse_of: :person
+
+	validates :name, presence: true
+	validates :type_person, presence: true
+	
+	has_many :contacts
+
+	before_save :default_values
+
+	def default_values
+		self.trash ||= 0
+	end
+
 	def toTrash
 		self.update(trash: 1)
 	end
+
 	def restore
 		self.update(trash: 0)
 	end
@@ -12,5 +27,9 @@ class Person < ActiveRecord::Base
 
 	def self.notDeleted
 		Person.where(trash: 0)
+	end
+
+	def nameSurname
+		nameSurname = "#{self.name}" + (self.surname.nil? ? "" : " #{self.surname}")
 	end
 end
